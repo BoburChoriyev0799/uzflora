48d35994cdb54a3a4477e1ed83afc05e100fd163
BoburChoriyev0799 <boburchoriyev465@gmail.com>
2025-07-26 19:31:42 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Asosiy marshrut
 app.get("/", (req, res) => {
   res.send("Uzflora server ishlayapti!");
 });
