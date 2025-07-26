db801adf77a6a12a356e6ca788f97c14da370117
BoburChoriyev0799 <boburchoriyev465@gmail.com>
2025-07-26 19:07:36 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Asosiy marshrut
+app.get("/", (req, res) => {
+  res.send("Uzflora server ishlayapti!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
